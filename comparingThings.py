63dd14107997daaad8079a9b434571906894f6ea
joaq3rd <58320512+joaq3rd@users.noreply.github.com>
2023-01-07 12:32:47 -0800

    Update comparingThings.py

--- comparingThings.py
+++ comparingThings.py
@@ -3,3 +3,7 @@ print("cat" == "dog") #False
 print(1 != 2) #True
 print(1 < "1") #TypeError because integer and strings
 print(1 == "1") #False
+
+print("Yellow" > "Cyan" and "Brown" > "Magenta") #False
+print(25 > 50 or 1 != 2) #True
+print(not 42 == "Answer") #True
