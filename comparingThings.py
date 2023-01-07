65f6b833459815c48d90299243ce3b50a56ea82c
joaq3rd <58320512+joaq3rd@users.noreply.github.com>
2023-01-07 12:44:08 -0800

    Update comparingThings.py

--- comparingThings.py
+++ comparingThings.py
@@ -7,3 +7,10 @@ print(1 == "1") #False
 print("Yellow" > "Cyan" and "Brown" > "Magenta") #False
 print(25 > 50 or 1 != 2) #True
 print(not 42 == "Answer") #True
+
+
+#When a return statement is executed, the function exits, so that the code that follows doesn't get executed.
+def is_even(number):
+  if number % 2 == 0
+    return True
+  return False
